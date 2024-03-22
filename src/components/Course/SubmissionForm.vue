e0e9a7fd8fd522368d5b6d00891c55d7394bdbb9
nhiepchau <nhiepbaochau@gmail.com>
2024-03-22 22:00:48 +0700

    Minors

--- src/components/Course/SubmissionForm.vue
+++ src/components/Course/SubmissionForm.vue
@@ -1,17 +1,29 @@
 <template>
-    <v-tabs>
+    <v-tabs v-model="tab">
         <v-tab class="text-none" value="Submissions">
             <v-icon>fas fa-circle-check</v-icon>&nbsp;
             Submissions</v-tab>
         <v-tab class="text-none" value="LODistribution">LO Distribution</v-tab>
     </v-tabs>
-    <v-window class="mt-3">
+    <v-window class="mt-3" v-model="tab">
         <v-window-item value="Submissions">
-
+            <div class="mt-3">
+                <h3>Semester</h3>
+                <p>Exercise for each semester</p>
+            </div>
         </v-window-item>
         <!-- Break tabs -->
         <v-window-item value="LODistribution">
-
+            <div class="mt-3">
+                <h3>Distribution</h3>
+                <p>Exercise for each semester</p>
+            </div>
         </v-window-item>
     </v-window>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const tab = ref<String>('');
+</script>
