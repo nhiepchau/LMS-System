57b38119aa5ffcb7755b40c24ad5f25b6e2d2eb2
nhiepchau <nhiepbaochau@gmail.com>
2024-05-04 17:41:27 +0700

    Unhide /dist folder

---
+++ dist/assets/NotFound-kPzPJr5Z.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as o,w as a,s as c,b as t}from"./index-2v8kRtHD.js";const r={},n=t("div",{class:"mt-lg-16 text-center"},[t("h1",{class:"text-error text-lg-h1 font-weight-bold mb-5"},"404"),t("h1",{class:"text-error"},"Page Not Found")],-1);function _(l,d){return s(),o(c,{class:"h-100"},{default:a(()=>[n]),_:1})}const h=e(r,[["render",_]]);export{h as default};
